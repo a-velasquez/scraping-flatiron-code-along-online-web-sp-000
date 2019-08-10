789dccc0dcb934c73792aa3e2b5149d8d2721efb
Alex Velasquez <jtinnesco@gmail.com>
2019-08-10 23:45:26 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,6 +19,12 @@ class Scraper
     def get_courses
       self.get_page.css(".post")
     end
+    
+    def make_courses
+      self.get_courses.each do |post|
+        course = Course.new 
+        course.title = post.
+    end
   
     def print_courses
     self.make_courses
